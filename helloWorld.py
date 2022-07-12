ac3c30addbf01aee337d31f900286d935c65985b
Caleb Herring <cherring1@harding.edu>
2022-07-12 14:16:59 -0400

    Hello World commit

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 14:04:50 2022
+
+@author: Caleb Herring
+"""
+
+# Just a script that print hello world. A classic first in Software Engineer
+
+print("hello world")
