0c7eca567eb20887e746a1ed5867c929b5a54856
Caleb Herring <cherring1@harding.edu>
2022-07-12 14:35:14 -0400

    Add new file and list of numbers

---
+++ orderedList.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 14:10:53 2022
+
+@author: Caleb Herring
+"""
+
+#The Goal of this program is to read in a file containing numbers and order them in Desc and Asc order
+
+ListOfNums = open["100Random9DigitNumbers", "r"]
+
